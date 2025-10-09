95862846a47020a25e2484c8f5afbc0bfedfc2fb
mukuwab <mukuwab@yahoo.com>
2025-10-09 15:16:31 -0500

    changes

---
+++ SimpleExamples/layers.js
@@ -0,0 +1,15 @@
+//This works...
+
+    class Cake {
+        constructor(lyr) {
+            //lyr is a parameter
+            this.layers = lyr + 1;
+            //layers is a PROPERTY being defined using the lyr parameter (plus 1)
+
+        }
+    }
+
+    var result = new Cake(1); //new cake object made, setting lyr = 1
+    //so the layers property will equal 2
+
+    console.log(result.layers);//print the layers property of the new result object
