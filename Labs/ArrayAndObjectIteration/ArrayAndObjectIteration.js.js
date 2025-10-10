416798fd1b79137832886c04ed27336983bd469e
mukuwab <mukuwab@yahoo.com>
2025-10-09 21:46:29 -0500

    changes

--- Labs/ArrayAndObjectIteration/ArrayAndObjectIteration.js.js
+++ Labs/ArrayAndObjectIteration/ArrayAndObjectIteration.js.js
@@ -1,13 +1,26 @@
 // Task 1
 // WRITE YOUR CODE HERE - Create the logDairy function and use a for...of loop to log each item in the dairy array
 var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
-function logDairy(array) {
-    for (items of array) {
-        console.log(items);
+function logDairy() {
+    for (item of dairy) {
+        console.log(item);
     }
+    //dairy is a global variable so it is included in the scope of this function
+        //no need to pass it in
 }
 
-logDairy(dairy);
+logDairy();
+
+
+//quick scope example
+var fruit = "apple"; //global variable
+
+function printFruit(){
+    console.log("\n",fruit,"\n");
+    //prints global variable without parameters
+}
+
+printFruit();
 
 // Task 2
 // WRITE YOUR CODE HERE - Create the birdCan function and use a for...of loop to log bird object's own properties
