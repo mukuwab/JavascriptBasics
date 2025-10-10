9fd37edf01ba58e4bf06f45276a0abfa082aa89b
mukuwab <mukuwab@yahoo.com>
2025-10-09 21:29:23 -0500

    changes

--- Labs/ArrayAndObjectIteration/ArrayAndObjectIteration.js.js
+++ Labs/ArrayAndObjectIteration/ArrayAndObjectIteration.js.js
@@ -21,14 +21,35 @@ bird.canFly = true;
 bird.hasFeathers = true;  
 
 function birdCan() {
+    
+    console.log("\n");
+
     //loop through the object's properties, return properties + values
     for (const key of Object.keys(bird)) {
         console.log(key, ": ", bird[key]);
         //key --> returns keys
         //bird[key] --> returns the value of the key
     }
+
+    console.log("\n");
 }
 
 
-// Task 3
+birdCan();
+
+
+// Task 3: Iterate Over an Object and Its Prototype Properties
 // WRITE YOUR CODE HERE - Create the animalCan function and use a for...in loop to log all bird properties, including inherited ones
+
+
+function animalCan() {
+    console.log("\n");
+
+    for (items in bird) {
+        //will iterate through bird properties & its protoptype (animal)
+        console.log(items + ": ", bird[items]);
+        //use bird[items] to return the values
+    }
+}
+
+animalCan();
