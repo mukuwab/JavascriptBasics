941a7729e2d9326a58ff2c0dc0f26790c305e47c
mukuwab <mukuwab@yahoo.com>
2025-10-19 12:38:13 -0500

    changes

---
+++ SimpleExamples/ex1.js
@@ -0,0 +1,11 @@
+console.log(result);
+var result = 7;
+
+//this will be undefined since result has not yet
+//been declared
+
+
+var cat = {}
+cat["sound"] = "meow"
+var catSound = "purr"
+console.log(cat.sound)
